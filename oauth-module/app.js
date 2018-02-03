6a179816e5049ccea119ffb1cd60bbdfee43f23c
kalyansri2508 <kalyansri2508@gmail.com>
2018-02-03 13:39:36 +0800

    Google Oauth2 implemented with login,logout routes

---
+++ oauth-module/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+
+app.set('view engine', 'ejs');
+app.use('/public',express.static('./public'));
+
+const keys = require('./config/keys.js');
+app.use(cookieSession({
+  name: 'session',
+  keys: [keys.MY_SECRET],
+
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+const passportConfig = require('./config/passport-config.js');
+
+const authRoutes = require('./routes/auth-routes');
+app.use('/auth',authRoutes);
+const profileRoutes = require('./routes/profile-routes');
+app.use('/profile',profileRoutes);
+
+app.get('/home',(req,res)=>{
+  console.log(req.user);
+  res.render('home.ejs');
+});
+
+const port = require('./config/server-config.js').port;
+app.listen(port,()=>{
+  console.log("listening on port :"+port);
+});
---
+++ oauth-module/routes/auth-routes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const passport = require('passport');
+
+router.get('/login',(req,res)=>{
+    res.render('login.ejs');
+});
+
+router.get('/logout',(req,res)=>{
+    req.logout();
+    res.redirect('/home');
+});
+
+router.get('/google',passport.authenticate('google',{
+  scope:['profile']
+}));
+
+
+router.get('/google/redirect',passport.authenticate('google',{
+		successRedirect: '/profile',
+		failureRedirect: '/auth/login'
+}));
+
+module.exports = router;
